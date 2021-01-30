e1b2d489f5af5172ac137c5eebeb5048223e56c7
Leyda <48352107+leydaaltun@users.noreply.github.com>
2021-01-30 10:12:52 +0300

    added delete button

--- todo.js
+++ todo.js
@@ -2,87 +2,98 @@ let toDos = [
 
 ]
 
+let todoWrapper = []
 
-
-if(localStorage.getItem('savedToDos')) {
+if (localStorage.getItem('savedToDos')) {
     const savedToDos = localStorage.getItem('savedToDos')
+        //console.log(savedToDos)
     const parsed = JSON.parse(savedToDos)
-    console.log(parsed)
+        //console.log(parsed)
     toDos = parsed
 }
 
 
-function getToDo(toDo,index) {
-    console.log(index,toDo)
+function getToDo(toDo, index) {
+    //console.log(index, toDo)
     const text = toDo.text
     const isDone = toDo.isDone
 
     let className = ''
 
-    if(isDone === true) {
+    if (isDone === true) {
         className = 'done'
-    } 
-    
-
-    return (`
+    }
+    return (`<div class='TodoWrapper data-index=${index}'>
     <div class='todo ${className}' data-index='${index}'>
         ${text}
-    </div>`)
+    </div>
+    <button class="deleteImg"></button>
+    </div>
+    `)
 
 }
 
-//console.log(getToDo(toDos[1]))
+//console.log(getToDo(toDos[5]))
+
+
 
 const parent = document.querySelector('.todo-list')
-//parent.innerHTML = getToDo(toDos[1])
+    //parent.innerHTML = getToDo(toDos[1])
 
 const toDoCount = document.querySelector('.todoCount')
-//console.log(toDoCount)
+    //console.log(toDoCount)
 
 function updateHTML() {
     let html = ''
 
-    toDos.forEach((item,index) => {
-        const toDo = getToDo(item,index)
+    toDos.forEach((item, index) => {
+        const toDo = getToDo(item, index)
         html += toDo
     })
 
-    console.log(html)
+    //console.log(html)
     parent.innerHTML = html
 
-    const newToDo = toDos.filter(function(toDo){
-        if(toDo.isDone === false) {
+
+    const newToDo = toDos.filter(function(toDo) {
+
+        if (toDo.isDone === false) {
             return true
-        } else {
+        } else if (toDo.isDone === true) {
             return false
+        } else {
+            return
         }
 
-        
+
     })
-    console.log(newToDo) 
+
+
+
+
+    //console.log(newToDo)
     toDoCount.innerHTML = newToDo.length
 
-    
-}
 
+}
 
 
 updateHTML()
 
 const toDoElems = [...document.querySelectorAll('.todo')]
-//console.log(toDoElems)
+    //console.log(toDoElems)
 
-// toDoElems.forEach(toDo => {
-//     //console.log(toDo)
-//     toDo.addEventListener('click',handleClick)
-// }) 
+//toDoElems.forEach(toDo => {
+// console.log(toDo)
+//toDo.addEventListener('click', handleClick)
+//})
 
-parent.addEventListener('click',checkIfToDo)
+parent.addEventListener('click', checkIfToDo)
 
 function checkIfToDo(event) {
     //console.log(event.target.classList)
 
-    if(event.target.classList.contains('todo')) {
+    if (event.target.classList.contains('todo')) {
         handleClick(event.target)
     }
 
@@ -90,51 +101,68 @@ function checkIfToDo(event) {
 
 function handleClick(element) {
     const index = element.dataset.index
-    //console.log(index)
+        //console.log(index)
     let isDone = toDos[index].isDone
-
-    if(isDone) {   // this means === true
+        //console.log(isDone)
+    if (isDone) { // this means === true
         toDos[index].isDone = false
-        //console.log(true)
+            //console.log(true)
     } else {
         toDos[index].isDone = true
-        //console.log(false)
+            //console.log(false)
     }
-    console.log(toDos)
-    
+    //console.log(toDos)
+
+
+
     const json = JSON.stringify(toDos)
-    localStorage.setItem('savedToDos',json)
+    localStorage.setItem('savedToDos', json)
 
     updateHTML()
 }
 
+
+const container = document.querySelector('.container')
+    //console.log(container)
+container.addEventListener('click', deleteTodo)
+
+function deleteTodo(event) {
+    //console.log(event)
+    const parent = event.target.parentElement
+        //console.log(parent)
+    if (event.target.classList.contains('deleteImg')) {
+        parent.remove()
+
+    }
+
+}
+
+
 const form = document.querySelector('form')
-//console.log(form)
-form.addEventListener('submit',handleSubmit)
+    //console.log(form)
+form.addEventListener('submit', handleSubmit)
 
 const input = document.querySelector('input')
-//console.log(input)
+    //console.log(input)
 
 function handleSubmit(event) {
     event.preventDefault()
     console.log(input.value)
-    if(input.value !== '') {    
-        const object = 
-        {
-            text: input.value ,
+    if (input.value !== '') {
+        const object = {
+            text: input.value,
             isDone: false,
         }
-    toDos.push(object)
-    console.log(toDos)
+        toDos.push(object)
+        console.log(toDos)
 
-    const json = JSON.stringify(toDos)
-    localStorage.setItem('savedToDos',json)
+        const json = JSON.stringify(toDos)
+        localStorage.setItem('savedToDos', json)
 
-    updateHTML()
+        updateHTML()
 
-    input.value = ''
+        input.value = ''
+    }
+
+
 }
-
-    
-}
-
